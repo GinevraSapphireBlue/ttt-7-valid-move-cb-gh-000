40f52d811375e8e8dc31695fca3feec331b236c5
Anna Oleksiak <anna.ol3ksiak@gmail.com>
2018-05-15 22:16:57 +0000

    automatically backed up by learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -7,7 +7,7 @@ end
 def position_taken? (board, position)
   if board[position].strip == ""
     false
-  elsif board[position] == 'X' || board[postion] == 'O'
+  elsif board[position] == 'X' || board[position] == 'O'
     true
   else
     false
